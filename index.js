0afd1888ac0e44ff749792f6119887ed594dfaaa
Cody <cjgordon@gmail.com>
2017-09-09 21:27:27 -0400

    v1.1

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require('dotenv').load();
 const redash = require('./lib/redash');
-const sf = require('./lib/salesforce');
+const salesforce = require('./lib/salesforce');
+const { logger, finished } = require('./lib/handlers');
 
 (async function init() {
-  await sf.connect();
+  logger.info('starting upload');
+  await salesforce.connect();
   const contactsQuery = await redash.fetchQuery(2928);
-  await sf.upsertContacts(contactsQuery.data.rows);
+  await salesforce.upsertContacts(contactsQuery.data.rows);
+  finished();
 }());
