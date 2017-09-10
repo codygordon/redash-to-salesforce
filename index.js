60ddd8c2127966f3414743a90154ee58896f3bc2
Cody <cjgordon@gmail.com>
2017-09-09 22:23:11 -0400

    add NODE_ENV

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const redash = require('./lib/redash');
 const salesforce = require('./lib/salesforce');
 const { logger, finished } = require('./lib/handlers');
 
+const env = process.env.NODE_ENV;
+
 (async function init() {
-  logger.info('starting upload');
+  logger.info('starting upload in', env);
   await salesforce.connect();
   const contactsQuery = await redash.fetchQuery(2928);
   await salesforce.upsertContacts(contactsQuery.data.rows);
