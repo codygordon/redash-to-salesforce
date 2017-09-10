4174a73355248b865176f265621f9919e5c679f6
Cody Gordon <cjgordon@gmail.com>
2017-09-10 13:29:00 -0400

    webpack dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './lib/index.js',
@@ -7,4 +8,10 @@ module.exports = {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new Dotenv({
+      path: '.env',
+      safe: true,
+    }),
+  ],
 };
